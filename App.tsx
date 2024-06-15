406583b1c9e96c7c2c3156848073fa21386a1ea4
TI-1986 <tiagohanry@gmail.com>
2024-06-15 19:55:12 -0300

    Fix: correção de bug na exibição das imagens do expo go

--- App.tsx
+++ App.tsx
@@ -1,20 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native';
+
+
+import verde  from './assets/pictures/verde.png';
+import yellow  from './assets/pictures/amarelo.png';
+
 
 export default function App() {
+
+  const [isActive, setisActive] =  useState(false)
+ function  handleSymbol(){
+  setisActive((oldValue:boolean) => {
+      return !oldValue
+    })
+    console.log(isActive)
+  }  
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={isActive ? styles.containerv : styles.containery}>
+      <TouchableOpacity onPress={handleSymbol} >
+      
+        <Image
+        source={isActive ? verde : yellow}
+        style={styles.image}
+        
+      />
+        </TouchableOpacity>     
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  containerv: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
+    textAlign: 'center',   
   },
+  containery: {
+    flex: 1,
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+    textAlign: 'center',   
+  },
+
+  image:{
+    width: 250,
+    height: 250,
+  },
+
+   
 });
